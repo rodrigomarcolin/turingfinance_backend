2e02b6588736eb85bd66d0853f1070726debb4e3
rodrigomarcolin <rodrigosalesmarcolin@gmail.com>
2023-06-13 23:23:34 -0300

    add docstr to metrics and plots

---
+++ tfinance_backend/tquant_api/metrics.py
@@ -0,0 +1,8 @@
+"""
+Este arquivo contém funções para calcular métricas sobre ativos financeiros utilizando a biblioteca turingquant.
+
+O frontend e o backend podem ser gerados a partir dessas funções, possibilitando que os usuários vejam os resultados
+desses cálculos em uma interface de usuário, sem a necessidade de escrever código adicional.
+
+Os inputs de cada função devem ser rigorosamente documentados, bem como o seu output.
+"""
--- tfinance_backend/tquant_api/plots.py
+++ tfinance_backend/tquant_api/plots.py
@@ -1,16 +1,59 @@
+"""
+Este arquivo contém funções para plotar gráficos interativos usando a biblioteca Plotly Express.
+
+O frontend e o backend podem ser gerados a partir dessas funções, possibilitando que os usuários vejam os resultados
+desses gráficos em uma interface de usuário, sem a necessidade de escrever código adicional.
+
+Os inputs de cada função devem ser rigorosamente documentados, bem como o seu output.
+"""
+
+
 import plotly.express as px
 from plotly.offline import plot
 import json, plotly
+import pandas as pd
 
 
-def plot_histogram(df, title, nbins=100):
+def plot_histogram(df: pd.DataFrame, title: str, nbins: int = 100) -> str:
+    """
+    Plota um histograma interativo usando a biblioteca Plotly Express.
+
+    Args:
+        df (pd.DataFrame): O DataFrame contendo os dados a serem plotados.
+        title (str): O título do histograma.
+        nbins (int, optional): O número de bins para o histograma. O padrão é 100.
+
+    Returns:
+        str: Uma string contendo o JSON que representa o gráfico plotado.
+    """
+    
+    # TODO: refatorar para receber o pd.Series a ser plotado em vez de receber o df, para
+    # assim permitir maior flexibilidade.
+
     fig = px.histogram(df['Adj Close'].pct_change().dropna(), nbins=nbins, title=title)
     plot_div = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
 
     return plot_div
 
-def plot_rolling_std(df, title):
+
+def plot_rolling_std(df: pd.DataFrame, title: str) -> str:
+    """
+    Plota o desvio padrão móvel de uma série temporal usando a biblioteca Plotly Express.
+
+    Args:
+        df (pd.DataFrame): O DataFrame contendo os dados a serem plotados.
+        title (str): O título do gráfico.
+
+    Returns:
+        str: Uma string contendo o JSON que representa o gráfico plotado.
+    """
+
+    # TODO: refatorar para receber o pd.Series a ser plotado em vez de receber o df, para
+    # assim permitir maior flexibilidade.
+
     fig = px.line(df['Close'].rolling(90).std(), title=title)
     plot_div = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
 
     return plot_div
+
+
